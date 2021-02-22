1d65bfcb918192676c753fdf46a69f354e95f6fd
Yaming Gong <ymgongcn@gmail.com>
2021-02-22 09:48:46 +0800

    Add flask db upgrade support

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 
-from app import create_app
+from app import create_app, db
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run()
